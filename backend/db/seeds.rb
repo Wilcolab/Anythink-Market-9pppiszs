0e583a7c7e621337ec154907c8e51b1a2f1238a6
Eugenio Garcia <eugenio5039@gmail.com>
2024-01-30 23:04:11 +0000

    Add - seed data for users and items

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Load Users
+for i in 1..100
+    user = User.new(email: "test#{i}@test.com", username: "test#{i}", password: 'password', password_confirmation: 'password')
+    user.save unless user.validate != true
+end
+
+# Load Products and Comments
+for i in 1..100
+    item = Item.new(title: "Title#{i}", slug: "Slug#{i}", description: "desc")
+    item.user = User.first
+    comment = Comment.new(body: "body for item")
+    comment.user = User.first
+    comment.item = item
+    if item.validate 
+        item.save
+        comment.save
+    end
+end
